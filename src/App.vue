497d4c8ed0d963291443ed04202e6c799df5cc47
Marina Yanysheva <yme@incom-sr.ru>
2023-12-22 10:53:33 +0300

    realize addTodo

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,12 @@
 	<AppFilters />
 
 	<main class="app-main">
-		<AppTodoList />
-		<AppAddTodo />
+		<AppTodoList
+			:todos="todos"
+			@toggle-todo="toggleTodo"
+			@remove-todo="removeTodo"
+		/>
+		<AppAddTodo @add-todo="addTodo" />
 	</main>
 
 	<AppFooter />
@@ -12,11 +16,16 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import { Todo } from "@/types/Todo";
 import AppHeader from "@/components/AppHeader.vue";
 import AppFilters from "@/components/AppFilters.vue";
 import AppTodoList from "@/components/AppTodoList.vue";
 import AppAddTodo from "@/components/AppAddTodo.vue";
 import AppFooter from "@/components/AppFooter.vue";
+
+interface State {
+	todos: Todo[];
+}
 
 export default defineComponent({
 	components: {
@@ -25,6 +34,29 @@ export default defineComponent({
 		AppTodoList,
 		AppAddTodo,
 		AppFooter,
+	},
+	data(): State {
+		return {
+			todos: [
+				{ id: 0, text: "Learn the basics of Vue", completed: true },
+				{ id: 1, text: "Learn the basics of Typescript", completed: false },
+				{ id: 2, text: "Subscribe to the channel", completed: false },
+			],
+		};
+	},
+	methods: {
+		toggleTodo(id: number) {
+			const targetTodo = this.todos.find((todo: Todo) => todo.id === id);
+			if (targetTodo) {
+				targetTodo.completed = !targetTodo.completed;
+			}
+		},
+		removeTodo(id: number) {
+			this.todos = this.todos.filter((todo: Todo) => todo.id !== id);
+		},
+		addTodo(todo: Todo) {
+			this.todos.push(todo);
+		},
 	},
 });
 </script>
